27882d91d40acc6bb723fa5c8630afb60cde1122
sump3t <keko1504@outlook.com>
2025-07-18 08:08:32 -0400

    test

--- config/mainMenu/hotDeals.lua
+++ config/mainMenu/hotDeals.lua
@@ -1,16 +1,20 @@
 -- CANT MORE THAN 3 ITEMS
 
 AK4Y.hotDeals = {
-    -- {
-    --     hotItemPageValue = "vehicle-menu",
-    --     hotItemId = 5 -- unique id of the item in the page
-    -- },
-    -- {
-    --     hotItemPageValue = "item-menu",
-    --     hotItemId = 7 -- unique id of the item in the page
-    -- },
-    -- {
-    --     hotItemPageValue = "money-menu",
-    --     hotItemId = 6 -- unique id of the item in the page
-    -- },
+    {
+        hotItemPageValue = "vehicle-menu",
+        hotItemId = 5 -- unique id of the item in the page
+    },
+    {
+        hotItemPageValue = "item-menu",
+        hotItemId = 1 -- unique id of the item in the page
+    },
+    {
+        hotItemPageValue = "item-menu",
+        hotItemId = 7 -- unique id of the item in the page
+    },
+    {
+        hotItemPageValue = "item-menu",
+        hotItemId = 2 -- unique id of the item in the page
+    },
 }
